07cdf6bc695bd81d84d79309bf1acf867576d854
Chelsea Wetzel <chelseagilmore14@gmail.com>
2020-01-21 13:57:59 -0500

    table

--- data/migrations/20200121132753_songs_table.js
+++ data/migrations/20200121132753_songs_table.js
@@ -1,8 +1,28 @@
 
 exports.up = function(knex) {
-  
+    // REMEMBER the return 
+    return knex.schema.createTable('songs', tbl => {
+         // id column, primary key, auto increment
+         tbl.increments();
+
+         // string for the name
+         tbl.string('name', 255).index();
+
+         // duration
+         tbl.integer('duration');
+
+         // artist
+         tbl.string('artist').index();
+
+         tbl.boolean('favorite').defaultTo(false);
+         // most RDBMS store 1 for true and 0 for false
+
+        // adds created_at and updated_at columns
+         tbl.timestamps(true, true);
+    })
 };
 
+// undo the changes from the up function (rollback)
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists('songs')
 };
